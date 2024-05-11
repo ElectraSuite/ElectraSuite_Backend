c25d56eacba426799e3ebadcb6fa2d3c67621845
HridayAg0102 <hridayagrawal0102@gmail.com>
2024-05-11 12:22:29 +0530

    updated schema

--- electra_suite/handlers/handler_functions.py
+++ electra_suite/handlers/handler_functions.py
@@ -4,11 +4,30 @@ from electricpy import dynetz
 
 
 def handle_phasor_plot(args):
+    args = [[float(j) for j in i.split(",")] for i in args]
     phasors_ = phasors.phasorlist(args)
     plt = visu.phasorplot(phasors_, legend = True, labels = args)
     plt.show()
 
 def dynetz_handler(args):
-    dic = {"delta" : args[0] if not "NA" else None,
-           "wye" : args[1] if not "NA" else None}
+    # splitting each argument on basis of ",". "#" is ignored.
+    args = [i.split(",") for i in args]
+    for i in range(len(args)):
+        if args[i][0] == "#":
+            args[i] = None
+        else:
+            arr = args[i]
+            args[i] = [complex(i) for i in arr]
+
+    dic = {"delta" : args[0],
+           "wye" : args[1],
+           "round": 3}
     return dynetz(**dic)
+
+if __name__ == "__main__":
+    args_phasor = ["68,0", "72,120", "53,-120"]
+    handle_phasor_plot(args_phasor)
+
+    args_dynetz = ["3+2j,5+8j,3j", "#"]
+    print(dynetz_handler(args_dynetz))
+
